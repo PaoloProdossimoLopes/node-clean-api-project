4c3df735c7703996e7f8f8b541113ac18b73cd8e
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-10-20 08:08:10 -0300

    refactor: ensure `SignUpController.handle` implement correctc interfaces'

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,11 +1,10 @@
-import { SignUpController } from "./signup"
+import { SignUpController } from './signup'
 
 describe('SignUpController', () => {
   test('Should return 400 if no nome is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        // name: 'any_email',
         email: 'any_password',
         password: 'any_pasword_confimation'
       }
@@ -20,7 +19,6 @@ describe('SignUpController', () => {
     const httpRequest = {
       body: {
         name: 'any_email',
-        // email: 'any_password',
         password: 'any_pasword_confimation'
       }
     }
