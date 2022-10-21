87c8c2c586951e94c20ad24bd2d3c437c0b2a34d
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-10-20 21:58:27 -0300

    feat: ensure `SignUpController` response with statusCode 400 and `MisingParrmsError`

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,17 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HTTPRequest): HTTPResponse {
-    // if (!httpRequest.body.name) {
-    //   const error = new MissinParamsError('nome')
-    //   return badRequest(error)
-    // }
-
-    // if (!httpRequest.body.email) {
-    //   const error = new MissinParamsError('email')
-    //   return badRequest(error)
-    // }
-
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissinParamsError(field))
