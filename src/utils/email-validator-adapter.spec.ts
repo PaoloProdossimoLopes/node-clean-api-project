80eb8e63d98323cb7b06b6deed2d8cb1cc9c0762
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-10-23 22:00:01 -0300

    refactor: replace magic strings

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -4,21 +4,21 @@ import validator from 'validator'
 describe('EmailValidatorAdapter', () => {
   test('Returns false if validator returns false', () => {
     const sut = makeSUT()
-    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('invalid_email@mail.com')
+    jest.spyOn(validator, kValidatorIsValidMethodName).mockReturnValueOnce(false)
+    const isValid = sut.isValid(makeInvalidEmail())
     expect(isValid).toBeFalsy()
   })
 
   test('Returns true if validator returns true', () => {
     const sut = makeSUT()
-    const isValid = sut.isValid('valid_email@mail.com')
+    const isValid = sut.isValid(makeValidEmail())
     expect(isValid).toBeTruthy()
   })
 
   test('Call validator with correct email', () => {
     const sut = makeSUT()
-    const validEmail = 'valid_email@mail.com'
-    const emailSpy = jest.spyOn(validator, 'isEmail')
+    const validEmail = makeValidEmail()
+    const emailSpy = jest.spyOn(validator, kValidatorIsValidMethodName)
     sut.isValid(validEmail)
     expect(emailSpy).toHaveBeenCalledWith(validEmail)
   })
@@ -28,6 +28,12 @@ describe('EmailValidatorAdapter', () => {
 const makeSUT = ((): EmailValiadatorAdapter => {
   return new EmailValiadatorAdapter()
 })
+
+const makeValidEmail = (): string => 'valid_email@mail.com'
+
+const makeInvalidEmail = (): string => 'invalid_email@mail.com'
+
+const kValidatorIsValidMethodName = 'isEmail'
 
 // @Test doubles
 jest.mock('validator', () => ({
