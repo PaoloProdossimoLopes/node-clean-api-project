127ef2aa07a2807b0f034a62bcf3b6e54a19fa15
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-10-23 21:52:50 -0300

    refactor: extract `EmailValidatorAdapter` instance into helper factoru method `makeSUT`

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,31 +1,37 @@
 import { EmailValiadatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
-jest.mock('validator', () => ({
-  isEmail (): boolean {
-    return true
-  }
-}))
-
 describe('EmailValidatorAdapter', () => {
   test('Returns false if validator returns false', () => {
-    const sut = new EmailValiadatorAdapter()
+    const sut = makeSUT()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBeFalsy()
   })
 
   test('Returns true if validator returns true', () => {
-    const sut = new EmailValiadatorAdapter()
+    const sut = makeSUT()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBeTruthy()
   })
 
   test('Call validator with correct email', () => {
-    const sut = new EmailValiadatorAdapter()
+    const sut = makeSUT()
     const validEmail = 'valid_email@mail.com'
     const emailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid(validEmail)
     expect(emailSpy).toHaveBeenCalledWith(validEmail)
   })
 })
+
+// @Helpers
+const makeSUT = ((): EmailValiadatorAdapter => {
+  return new EmailValiadatorAdapter()
+})
+
+// @Test doubles
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
