4611f18b6e29485355bc11117e9ab4fa6b0a8526
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-02 19:02:06 -0300

    refactor: extract `add` method into some small process to inprove redability

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,20 +2,31 @@ import { IAddAccountRepository } from '@/data/protocols/add-account-repository'
 import { IAccountModel } from '@/domain/models/account-model'
 import { AddAccountModel } from '@/domain/use-cases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { InsertOneResult } from 'mongodb'
 
 export class AccountMongoRepository implements IAddAccountRepository {
   async add (account: AddAccountModel): Promise<IAccountModel> {
     const collection = MongoHelper.getCollection('accounts')
     const result = (await collection.insertOne(account))
+    return await this.resultHandler(account, result)
+  }
+
+  private async resultHandler (account: AddAccountModel, result: InsertOneResult<Document>): Promise<IAccountModel> {
     if (result !== null) {
-      return new Promise((resolve) => resolve({
-        id: result.insertedId.toJSON(),
-        name: account.name,
-        email: account.email,
-        password: account.password
-      }))
-    } else {
-      return new Promise((resolve) => resolve(null))
+      const idRegistered = result.insertedId.toJSON()
+      const accountRegistered = this.map(account, idRegistered)
+      return new Promise((resolve) => resolve(accountRegistered))
+    }
+
+    return new Promise((resolve) => resolve(null))
+  }
+
+  private map (account: AddAccountModel, id: string): IAccountModel {
+    return {
+      id,
+      name: account.name,
+      email: account.email,
+      password: account.password
     }
   }
 }
