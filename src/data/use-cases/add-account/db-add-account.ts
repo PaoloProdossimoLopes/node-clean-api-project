df0a116c934708180f588e1c6a8dcfef7ca435d8
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-01 23:15:36 -0300

    feat: call repository to `addAccount` with encripted password

--- src/data/use-cases/add-account/db-add-account.ts
+++ src/data/use-cases/add-account/db-add-account.ts
@@ -1,16 +1,21 @@
 import { AddAccountModel, IAddAccount } from '../../../domain/use-cases/add-account'
 import { IAccountModel } from '../../../domain/models/account-model'
 import { IEncrypter } from '../../protocols/encrypt'
+import { IAddAccountRepository } from './db-add-account.spec'
 
 export class DBAddAccount implements IAddAccount {
   private readonly encrypter: IEncrypter
+  private readonly repository: IAddAccountRepository
 
-  constructor (encrypter: IEncrypter) {
+  constructor (encrypter: IEncrypter, repository: IAddAccountRepository) {
     this.encrypter = encrypter
+    this.repository = repository
   }
 
   async add (data: AddAccountModel): Promise<IAccountModel> {
-    await this.encrypter.encrypt(data.password)
+    const passwordEncrypted = await this.encrypter.encrypt(data.password)
+    const repositoryAccount = Object.assign({}, data, { password: passwordEncrypted })
+    await this.repository.add(repositoryAccount)
     return new Promise(resolve => resolve(null))
   }
 }
