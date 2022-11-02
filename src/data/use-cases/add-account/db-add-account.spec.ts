88cfcab9c0f4374846ff08f1efd6e060025c5b07
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-01 22:26:42 -0300

    refactor: extract model/ objects into factory method to improve redability

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -1,16 +1,13 @@
 import { DBAddAccount } from './db-add-account'
 import { IEncrypter } from '../../protocols/encrypt'
+import { AddAccountModel } from '@/domain/use-cases/add-account'
 
 describe('DBAddAccount Usecase', async () => {
   test('should call `Encripter` with correct password', async () => {
     const { sut, encripter } = makeEnviroment()
-    const account = {
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'valid_password'
-    }
+    const account = makeAccount()
     await sut.add(account)
-    expect(encripter.passwordRecieved).toEqual('valid_password')
+    expect(encripter.passwordRecieved).toEqual(makeValidPassword())
   })
 })
 
@@ -25,6 +22,18 @@ const makeEnviroment = (): Envirment => {
   const sut = new DBAddAccount(encripter)
   return { sut, encripter }
 }
+
+const makeAccount = (): AddAccountModel => {
+  return {
+    name: makeValidName(),
+    email: makeValidEmail(),
+    password: makeValidPassword()
+  }
+}
+
+const makeValidName = (): string => 'valid_name'
+const makeValidEmail = (): string => 'valid_email'
+const makeValidPassword = (): string => 'valid_password'
 
 // @Doubles
 class EncripterSpy implements IEncrypter {
