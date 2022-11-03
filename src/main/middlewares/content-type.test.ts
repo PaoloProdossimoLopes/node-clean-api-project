8edad2a3591fa9ecbe320ca20df65f02a4aa82af
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-03 15:02:56 -0300

    refactor: group midfleware export in a single file

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -11,4 +11,15 @@ describe('ContentTypeMiddleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('should return xml content type when forced', async () => {
+    app.get('/test_content_type__xml', (request, response) => {
+      response.type('xml')
+      response.send()
+    })
+
+    await request(app)
+      .get('/test_content_type__xml')
+      .expect('content-type', /xml/)
+  })
 })
