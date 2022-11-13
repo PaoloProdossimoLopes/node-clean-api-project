d006efa2150f4b33e22a677a9b5da521a9f56cf3
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-12 21:46:21 -0300

    feat: ensure `LoginController` calls `isValid` method from `IEmailValidator` with correct values'

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,14 +1,23 @@
+import { IEmailValidator } from './../../protocols/email-validator'
 import { MissinParamsError } from './../../errors/missin-params-error'
 import { HTTPRequest, HTTPResponse } from '@/presentation/protocols'
 import { IController } from './../../protocols/controller'
 import { badRequest } from '../../helpers/http-helper'
 
 export class LoginController implements IController {
+  private readonly validator: IEmailValidator
+
+  constructor (validator: IEmailValidator) {
+    this.validator = validator
+  }
+
   async handle (httpRequest: HTTPRequest): Promise<HTTPResponse> {
     const badRequestError = this.validadeRequiredFilds(httpRequest)
     if (badRequestError) {
       return badRequest(badRequestError)
     }
+
+    this.validator.isValid(httpRequest.body.email)
 
     const successObject = {
       statusCode: 200,
